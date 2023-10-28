2903b74636bfa8ed870e617e11f62cc932af9410
The-Joker <henos.misganaw@gmail.com>
2023-10-28 17:08:08 +0300

    Write a function that plays a single round of Rock Paper Scissors

--- app.js
+++ app.js
@@ -7,4 +7,33 @@ function getComputerChoice(){
     return choice;
 }
 
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissor") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    }
+    else{
+        return "Computer"
+    }
+}
 
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It's a Tie"
+    }
+    else if(result == "Player"){
+        return "You won!"
+    }
+    else{
+        return "You lost!"
+    }
+}
+
+getComputerChoice()
